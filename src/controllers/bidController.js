2a86f21e7cd699f31d12dfe6b8c55939379daf19
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 <amr.ister20@gmail.com>
2022-01-11 23:30:30 +0200

    Controller functions implemented

--- src/controllers/bidController.js
+++ src/controllers/bidController.js
@@ -5,11 +5,35 @@ import BidModel from '../repository/bidModel';
 const bidLogger = debug('app:bidController');
 
 const addNewBid = (req, res) => {
-
+  const { username, petId, bidValue } = req.body;
+  bidLogger(`${username}, ${petId}, ${bidValue}`);
+  const newBid = BidModel({ username, petId, value: bidValue });
+  newBid.save()
+    .then((bid) => {
+      bidLogger(`Success bid for pet:"${bid.peId}"`);
+      return res.json(bid);
+    })
+    .catch((err) => {
+      bidLogger(`Failed to add bid, Error: ${err.errors}"`);
+      res.status(405).json({
+        msg: 'Invalid input',
+      });
+    });
 };
 
 const listAllBid = (req, res) => {
-
+  const { petId } = req.body;
+  BidModel.find({ petId })
+    .then((bid) => {
+      bidLogger(`Success retrieval for pet:"${bid.peId}"`);
+      return res.json(bid);
+    })
+    .catch((err) => {
+      bidLogger(`Failed to list bids, Error: ${err.errors}"`);
+      res.status(405).json({
+        msg: 'Invalid input',
+      });
+    });
 };
 
 module.exports = {
